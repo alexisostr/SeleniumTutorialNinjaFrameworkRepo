f602ab8257a474fd55846fc56ee8a74bb16ba952
alexisostr <aostr78@gmail.com>
2024-02-16 18:20:26 -0500

    Update locator to @FindBy(css = "a[title = 'My Account']") 	WebElement myAccoutDropMenu;

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -7,12 +8,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
 	WebDriver driver;
+	WebDriverWait wait;
 
-	@FindBy(xpath = "//span[text()='My Account']")
+	@FindBy(css = "a[title = 'My Account']")
 	WebElement myAccoutDropMenu;
 	@FindBy(linkText = "Login")
 	WebElement loginLink;
@@ -29,18 +33,22 @@ public class HomePage {
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(this.driver, this);
+		wait=new WebDriverWait(driver, Duration.ofSeconds(20));
 	}
 
 	// Business Logic
 	
 	public LoginPage navigateToLoginPage() {
-		myAccoutDropMenu.click();
+		
+		wait.until(ExpectedConditions.elementToBeClickable(myAccoutDropMenu)).click();
+		//myAccoutDropMenu.click();
 		loginLink.click();
 		return new LoginPage(driver);
 	}
 	
 	public RegisterPage navigateToRegisterPage() {
-		myAccoutDropMenu.click();
+		wait.until(ExpectedConditions.elementToBeClickable(myAccoutDropMenu)).click();
+		//myAccoutDropMenu.click();
 		registerLink.click();
 		return new RegisterPage(driver);
 	}
